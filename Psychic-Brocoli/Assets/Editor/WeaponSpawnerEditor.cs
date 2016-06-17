01deab036effc0eaa4f37aae88909824a4441d7d
BigCheese <JDSZ@GMX.de>
2016-06-17 17:16:26 +0200

    Adjustable spawn area + tweaks

---
+++ Psychic-Brocoli/Assets/Editor/WeaponSpawnerEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(WeaponSpawner))]
+public class WeaponSpawnerEditor : Editor
+{
+
+    void OnSceneGUI()
+    {
+        WeaponSpawner myObj = (WeaponSpawner)target;
+        RaycastHit hitInfo;
+        Handles.color = Color.green;
+
+        Physics.Raycast(myObj.transform.position, Vector3.down, out hitInfo);
+        float dist = Vector3.Distance(myObj.transform.position, hitInfo.point);
+
+        if (dist > 0 && dist < 50)
+        {
+            Debug.DrawLine(myObj.transform.position, hitInfo.point);
+            Handles.DrawWireDisc(myObj.transform.position, Vector3.up, myObj.dropZoneRadius);
+        }
+
+    }
+
+}
---
+++ Psychic-Brocoli/Assets/Scripts/MouseInput.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MouseInput : MonoBehaviour 
+{
+
+	private Vector3 characterPosition;
+	private Plane cursorPlane;
+	private Camera mainCamera;
+	private GameObject currentLevel;
+	private Collider mouseCollider;
+	private Vector3 cursorPlanePosition;
+
+
+	public Vector3 cursorPosition;
+
+
+	void Awake()
+	{
+		cursorPlanePosition = Vector3.zero;
+		mainCamera = Camera.main;
+		currentLevel = GameObject.FindWithTag("Level");
+		mouseCollider = currentLevel.GetComponent<Collider>();
+
+		cursorPlane = new Plane(Vector3.up ,cursorPlanePosition);
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		FindCursorPosition();
+
+	}
+
+
+	//Could optimize the function. Right now the same function gets called 2 times with minor differences.
+	//But it works.
+	void FindCursorPosition()
+	{
+		Vector3 cursorScreenPosition = Input.mousePosition;
+
+		cursorPosition = ScreenPointToWorldPointOnPlane(cursorPlane ,cursorScreenPosition  , mainCamera);
+
+		Vector3 cursorOnColliderPosition = ScreenPointToWorldPointOnCollider(mouseCollider, cursorScreenPosition, mainCamera);
+
+		float correctionAngle;
+
+
+
+
+
+		correctionAngle = cursorOnColliderPosition.y - cursorPlanePosition.y;
+
+		cursorPlanePosition.y += correctionAngle;
+
+		cursorPlane.SetNormalAndPosition(Vector3.up, cursorPlanePosition);
+
+		TurnTowardsCursor(cursorPosition);
+
+
+
+
+
+
+		/*
+		if(Input.GetKey(KeyCode.R))
+		{
+			Debug.Log(cursorPosition);
+		}
+		*/
+	
+	}
+
+
+	void TurnTowardsCursor(Vector3 cursorPos)
+	{
+		float speed = 20f;
+		Vector3 targetDir = cursorPos - transform.position;
+		speed *= Time.deltaTime;
+		
+		//constrain rotation so it doesn't turn up and down
+		targetDir.y *= 0;
+		
+		//Handy that DrawRay
+		Debug.DrawRay(transform.position, targetDir, Color.red);
+		transform.rotation = Quaternion.LookRotation(targetDir);
+	}
+
+
+
+
+//Collider
+
+	Vector3 ScreenPointToWorldPointOnCollider(Collider collider, Vector3 screenPoint, Camera camera)
+	{
+		Ray ray = camera.ScreenPointToRay(screenPoint);
+
+		return ColliderRayIntersection(collider, ray);
+
+	}
+		                              
+	Vector3 ColliderRayIntersection(Collider collider, Ray ray)
+	{
+
+		RaycastHit hitInfo;
+		collider.Raycast(ray, out hitInfo, 300.0F);
+		return ray.GetPoint (hitInfo.distance);
+	}
+
+//plane
+
+	Vector3 ScreenPointToWorldPointOnPlane(Plane plane, Vector3 screenPoint, Camera camera)
+	{
+		
+		Ray ray = camera.ScreenPointToRay(screenPoint);
+		
+		return PlaneRayIntersection (plane, ray);
+	}
+
+	Vector3 PlaneRayIntersection(Plane plane, Ray ray)
+	{
+		float dist;
+		plane.Raycast (ray, out dist);
+		return ray.GetPoint (dist);
+	}
+
+}
