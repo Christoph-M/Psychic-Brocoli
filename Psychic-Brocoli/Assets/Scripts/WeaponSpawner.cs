db19632ec3f1b2a51035d5fa37d99d66fb8833ce
BigCheese <JDSZ@GMX.de>
2016-06-17 15:53:25 +0200

    Finished Weapon Spawner

--- Psychic-Brocoli/Assets/Scripts/WeaponSpawner.cs
+++ Psychic-Brocoli/Assets/Scripts/WeaponSpawner.cs
@@ -1,15 +1,141 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class WeaponSpawner : MonoBehaviour {
+public class WeaponSpawner : MonoBehaviour
+{
+    public GameObject[] weaponsPrefabs;
+    public int maxNumberOfWeapons = 10;
+    public Transform[] throwingLocations = new Transform[4];
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public int dropZoneRadius = 10;
+    public float airTime = 5;
+
+    GameObject[] spawnedWeapons;
+
+    void Start()
+    {
+        spawnedWeapons = new GameObject[maxNumberOfWeapons];
+
+        RandomisedPosition();
+
+        StartCoroutine(SpawnWeaponLoop());
+
+        //Debug.Log(spawnedWeapons.Length);
+    }
+
+    IEnumerator SpawnWeaponLoop()
+    {
+        while (true)
+        {
+            float waitTime;
+
+            if (spawnedWeapons.Length != maxNumberOfWeapons)
+                Array.Resize(ref spawnedWeapons, maxNumberOfWeapons);
+
+            int objectsInArray = CountArray(spawnedWeapons);
+
+            for (int i = objectsInArray; i < maxNumberOfWeapons; i++)
+            {
+                Vector3 targetPosition;
+                bool isPositionValid = false;
+
+                do
+                {
+                    targetPosition = RandomisedPosition();
+                    isPositionValid = CheckForWall(targetPosition);
+                } while (!isPositionValid);
+
+                Vector3 throwingLocation = throwingLocations[UnityEngine.Random.Range(0, throwingLocations.Length)].position;
+
+                GameObject newWeapon = (GameObject)Instantiate( weaponsPrefabs[UnityEngine.Random.Range(0, weaponsPrefabs.Length)],
+                                                                throwingLocation,
+                                                                Quaternion.identity );
+                SetupWeapon(newWeapon, targetPosition, throwingLocation);
+                
+                AddToArray(spawnedWeapons, newWeapon);
+                yield return new WaitForSeconds(UnityEngine.Random.Range(2, 6));
+            }
+            if (objectsInArray == maxNumberOfWeapons)
+                waitTime = 2f;
+            else
+                waitTime = 5f;
+
+
+            yield return new WaitForSeconds(waitTime);
+        }
+
+    }
+
+    void SetupWeapon(GameObject weapon, Vector3 target, Vector3 origin)
+    {
+        weapon.transform.rotation = Quaternion.LookRotation(target - origin, Vector3.up);
+
+        weapon.AddComponent<Rigidbody>();
+        weapon.GetComponent<Rigidbody>().velocity = CalculateTrajectory(origin, target, airTime);
+    }
+
+    int CountArray(GameObject[] array)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] != null)
+            {
+                if (array[i].tag == "Pickup")
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    void AddToArray(GameObject[] array, GameObject item)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == null)
+            {
+                array[i] = item;
+                break;
+            }
+        }
+    }
+
+    Vector3 RandomisedPosition()
+    {
+        Vector2 randomCircle = UnityEngine.Random.insideUnitCircle * dropZoneRadius;
+        Vector3 randomPos = new Vector3(transform.position.x + randomCircle.x, transform.position.y, transform.position.z + randomCircle.y);
+
+        print(randomPos);
+        return randomPos;
+    }
+
+    bool CheckForWall(Vector3 point)
+    {
+        RaycastHit hitInfo;
+        Physics.Raycast(point, Vector3.down, out hitInfo);
+
+        if (hitInfo.transform.tag == "Wall")
+            return true;
+        else
+            return false;
+    }
+
+    Vector3 CalculateTrajectory(Vector3 from, Vector3 to, float time)
+    {
+        Vector3 gravity = Physics.gravity;
+        Vector3 velocityNeeded = Vector3.zero;
+        //Vector3 dir = to - from;
+
+        //dir = Quaternion.LookRotation(to-from, Vector3.up) * dir;
+
+        velocityNeeded.z = (to.z - from.z) / time;
+        velocityNeeded.y = (to.y + 0.5f * gravity.y * time * time - from.y) / time;
+
+
+        return velocityNeeded;
+    }
+
 }
