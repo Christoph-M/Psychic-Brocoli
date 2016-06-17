c869c1281165eeefd06e13be6383ae79c5f3a521
icch_howw <davidlawrence@online.de>
2016-06-17 11:27:51 +0200

    Added weapons

---
+++ Psychic-Brocoli/Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    [HideInInspector]
+    public Vector3 direction;
+    public float speed;
+    public float bulletSpread = 0;
+	
+    void Start()
+    {
+        direction = Quaternion.Euler(0, Random.Range(-bulletSpread, bulletSpread), 0) * direction;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        transform.position += direction * speed * Time.deltaTime;
+	}
+}
--- Psychic-Brocoli/Assets/Scripts/Weapon.cs
+++ Psychic-Brocoli/Assets/Scripts/Weapon.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(MeshRenderer)), RequireComponent(typeof(MeshFilter))]
 public class Weapon : MonoBehaviour {
+
+    public Mesh weaponMesh;
+    public GameObject bulletPrefab;
+    public KeyCode shootKey;
 
 	// Use this for initialization
 	void Start () {
-	
+        GetComponent<MeshFilter>().mesh = weaponMesh;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if (Input.GetKeyDown(shootKey))
+        {
+            GameObject temp = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            temp.GetComponent<Bullet>().direction = transform.forward;
+        }
 	}
 }
